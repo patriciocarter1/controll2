2fa22a2d946681e2eb92f8048278e8383e787c22
patriciocarter1 <p.cartergamboa@gmail.com>
2015-10-03 19:32:58 -0300

    jaja
    a

--- acciones.R
+++ acciones.R
@@ -18,21 +18,21 @@ na <- data.frame(x, y, z)
 cor(na) #correlacion entre variables 
 
 # regresiones
-fit <- lm (accion$a.falabella ~ accion$r.falabella + accion$r.ipsa + accion$a.ipsa)
+fit <- lm (accion$r.falabella ~ accion$r.ipsa )
 summary (fit)
 
-fit <- lm (accion$a.entel ~ accion$r.entel + accion$r.ipsa + accion$a.ipsa)
+fit <- lm (accion$r.entel ~ accion$r.ipsa )
 summary (fit)
 
-fit <- lm (accion$a.salfacorp ~ accion$r.salcacorp + accion$r.ipsa + accion$a.ipsa)
+fit <- lm (accion$r.salcacorp ~ accion$r.ipsa )
 summary (fit)
 
 
-plot(accion$a.falabella,accion$a.ipsa)
+plot(accion$r.falabella,accion$r.ipsa)
 
-plot(accion$a.entel,accion$a.ipsa)
+plot(accion$r.entel,accion$r.ipsa)
 
-plot(accion$a.salfacorp,accion$a.ipsa)
+plot(accion$r.salcacorp,accion$r.ipsa)
 
 
 
